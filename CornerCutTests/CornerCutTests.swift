1f7ee12ab2dbe3cf564508080723be764d811e4a
Vignesh Mohan <vmohan5072@gmail.com>
2024-12-29 19:26:12 -0500

    Initial Commit

---
+++ CornerCutTests/CornerCutTests.swift
@@ -0,0 +1,17 @@
+//
+//  CornerCutTests.swift
+//  CornerCutTests
+//
+//  Created by Vignesh on 12/29/24.
+//
+
+import Testing
+@testable import CornerCut
+
+struct CornerCutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
